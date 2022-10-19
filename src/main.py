57c6437eebd626fb2a8c458c26ee555f8e7ae5c6
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2022-10-19 17:58:17 +0000

    POST y DELETE

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Planets, Characters, Favorites
 #from models import Person
+import json 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -47,8 +48,50 @@ def handle_hello():
 def get_user(user_id):
     userId = User.query.filter_by(id=user_id).first()
     usuario = userId.serialize()
-    print(usuario)
     return jsonify(usuario), 200
+
+
+# Alta de un usuario
+@app.route('/user', methods=['POST'])
+def addUser():
+    body = json.loads(request.data)
+
+    queryNewUser = User.query.filter_by(email=body["email"]).first()
+    
+    if queryNewUser is None:
+        new_user = User(name=body["name"], 
+        lastname=body["lastname"], 
+        username=body["username"], 
+        email=body["email"], 
+        password=body["password"])
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+        response_body = {
+            "msg": "Nuevo usuario creado" 
+        }
+        return jsonify(new_user.serialize()), 200
+    
+    response_body = {
+        "msg": "Usuario ya creado" 
+    }
+    return jsonify(response_body), 400
+
+# Borra un usuario
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def deleteUser(user_id):
+    userId = User.query.filter_by(id=user_id).first()
+  
+    if userId is None: 
+        response_body = {"msg": "Usuario no encontrado"}
+        return jsonify(response_body), 400
+
+    db.session.delete(userId)
+    db.session.commit()
+    response_body = {"msg": "Usuario borrado"}
+    return jsonify(response_body), 200
+
 
 ########################
 #       Planetas       #
@@ -58,7 +101,6 @@ def get_user(user_id):
 def all_planets():
     planetas = Planets.query.all()
     results = list(map(lambda x: x.serialize(), planetas))
-    print (results)
     return jsonify(results), 200
 
 # Muestra planetas por id
@@ -66,8 +108,46 @@ def all_planets():
 def planets_porId(planet_id):
     planeta = Planets.query.filter_by(id=planet_id).first()
     planet = planeta.serialize()
-    print(planet)
     return jsonify(planet), 200
+
+
+# Alta de un planeta
+@app.route('/planets', methods=['POST'])
+def addPlanets():
+    body = json.loads(request.data)
+
+    queryNewPlanet = Planets.query.filter_by(name=body["name"]).first()
+    
+    if queryNewPlanet is None:
+        new_planet = Planets(name=body["name"])
+        
+        db.session.add(new_planet)
+        db.session.commit()
+        
+        response_body = {
+            "msg": "Nuevo planeta creado" 
+        }
+        return jsonify(new_planet.serialize()), 200
+    
+    response_body = {
+        "msg": "Planeta ya creado" 
+    }
+    return jsonify(response_body), 400
+
+# Borra un Planeta
+@app.route('/planets/<int:planet_id>', methods=['DELETE'])
+def deletePlanet(planet_id):
+    planetId = Planets.query.filter_by(id=planet_id).first()
+  
+    if planetId is None: 
+        response_body = {"msg": "Planeta no encontrado"}
+        return jsonify(response_body), 400
+
+    db.session.delete(planetId)
+    db.session.commit()
+    response_body = {"msg": "Planeta borrado"}
+    return jsonify(response_body), 200
+
 
 ########################
 #       Personajes     #
@@ -77,7 +157,6 @@ def planets_porId(planet_id):
 def all_characters():
     personaje = Characters.query.all()
     results = list(map(lambda x: x.serialize(), personaje))
-    print (results)
     return jsonify(results), 200
 
 # Muestra personajes por id
@@ -85,9 +164,46 @@ def all_characters():
 def characters_porId(characters_id):
     personaje = Characters.query.filter_by(id=characters_id).first()
     char = personaje.serialize()
-    print(char)
     return jsonify(char), 200
 
+
+# Alta de un personaje
+@app.route('/characters', methods=['POST'])
+def add_Characters():
+    body = json.loads(request.data)
+
+    queryNewCharacter = Characters.query.filter_by(name=body["name"]).first()
+    
+    if queryNewCharacter is None:
+        new_charc = Characters(name=body["name"], 
+        lastName=body["lastName"])
+        
+        db.session.add(new_charc)
+        db.session.commit()
+        
+        response_body = {
+            "msg": "Nuevo personaje creado" 
+        }
+        return jsonify(new_charc.serialize()), 200
+    
+    response_body = {
+        "msg": "Personaje ya creado" 
+    }
+    return jsonify(response_body), 400
+
+# Borra un Personaje
+@app.route('/characters/<int:character_id>', methods=['DELETE'])
+def deletePersonaje(character_id):
+    personajeId = Characters.query.filter_by(id=character_id).first()
+  
+    if personajeId is None: 
+        response_body = {"msg": "Personaje no encontrado"}
+        return jsonify(response_body), 400
+
+    db.session.delete(personajeId)
+    db.session.commit()
+    response_body = {"msg": "Personaje borrado"}
+    return jsonify(response_body), 200
 
 ########################
 #       Favoritos      #
@@ -107,6 +223,102 @@ def favorits_porId(favorits_id):
     favorite = favorito.serialize()
     print(favorite)
     return jsonify(favorite), 200
+
+# Borra la lista de los favoritos
+@app.route('/favorite/<int:fav_id>', methods=['DELETE'])
+def deleteFavorite(fav_id):
+    favId = Favorites.query.filter_by(id=fav_id).first()
+  
+    if favId is None: 
+        response_body = {"msg": "Favoritos no encontrado"}
+        return jsonify(response_body), 400
+
+    db.session.delete(favId)
+    db.session.commit()
+   
+    response_body = {"msg": "Favorito borrado"}
+    return jsonify(response_body), 200
+
+
+# Agrega un nuevo favorito
+@app.route('/favorite', methods=['POST'])
+def add_Favorites():
+    body = json.loads(request.data)
+
+    # Chequeo de ids de usuarios, planetas y personajes. 
+    existeUser = User.query.filter_by(id=body["id_user"]).first()
+    existePlanet = Planets.query.filter_by(id=body["id_planets"]).first()
+    existePers = Characters.query.filter_by(id=body["id_characters"]).first()
+
+     # Si se ingresa usuario, planeta y personaje.
+    if existeUser: 
+        if existePlanet:
+            if existePers:
+                new_Favorito = Favorites(id_user=body["id_user"], 
+                id_planets=body["id_planets"], id_characters=body["id_characters"])
+            
+                db.session.add(new_Favorito)
+                db.session.commit()
+                
+                response_body = {
+                    "msg": "Favorito creado" 
+                }
+                return jsonify(response_body), 200
+            else: 
+                response_body = {"msg": "Personaje no creado"}
+                return jsonify(response_body), 200
+        else: 
+            response_body = {"msg": "Planeta no creado"}
+            return jsonify(response_body), 200
+    else: 
+        response_body = {"msg": "Usuario no creado"}
+        return jsonify(response_body), 200
+    #print(body["id_characters"])
+   
+    # Si se ingresa usuario y planeta solamente.
+    if existeUser:
+        if existePlanet:
+            new_Favorito = Favorites(id_user=body["id_user"], 
+            id_planets=body["id_planets"])
+        
+            db.session.add(new_Favorito)
+            db.session.commit()
+            
+            response_body = {
+                "msg": "Favorito creado" 
+            }
+            return jsonify(response_body), 200
+        else: 
+            response_body = {"msg": "Planeta no creado"}
+            return jsonify(response_body), 200
+    else: 
+        response_body = {"msg": "Usuario no creado"}
+        return jsonify(response_body), 200
+        
+    # Si se ingresa usuario y personaje solamente.
+    if existeUser:
+        if existePers:
+            new_Favorito = Favorites(id_user=body["id_user"], 
+            id_characters=body["id_characters"])
+        
+            db.session.add(new_Favorito)
+            db.session.commit()
+            
+            response_body = {
+                "msg": "Favorito creado" 
+            }
+            return jsonify(response_body), 200
+        else:
+            response_body = {"msg": "Personaje no creado"}
+            return jsonify(response_body), 200
+    else: 
+        response_body = {"msg": "Usuario no creado"}
+        return jsonify(response_body), 200
+   
+    response_body = {
+        "msg": "Error al agregar favoritos" 
+    }
+    return jsonify(response_body), 400
 
 
 # this only runs if `$ python src/main.py` is executed
--- src/models.py
+++ src/models.py
@@ -5,11 +5,11 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
-    lastName = db.Column(db.String(120), unique=False, nullable=False)
-    userName = db.Column(db.String(120), unique=True, nullable=False)
+    lastname = db.Column(db.String(120), unique=False, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    favoritoUser = db.relationship("Favorites", backref="user", lazy=True)
+    favoritoUser = db.relationship("Favorites", backref="user", cascade="all, delete-orphan", lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.id
@@ -18,8 +18,8 @@ class User(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "lastName": self.lastName,
-            "userName": self.userName,
+            "lastname": self.lastname,
+            "username": self.username,
             "email": self.email,
             #"favorito": self.favorito
         }
@@ -28,7 +28,7 @@ class Characters(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
     lastName = db.Column(db.String(120), unique=False, nullable=False)
-    favoritoCharacters = db.relationship("Favorites", backref="characters", lazy=True)
+    favoritoCharacters = db.relationship("Favorites", backref="characters", cascade="all, delete-orphan", lazy=True)
 
     def __repr__(self):
         return '<Characters %r>' % self.id
@@ -44,7 +44,7 @@ class Characters(db.Model):
 class Planets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
-    favoritoPlanta = db.relationship("Favorites", backref="planets", lazy=True)
+    favoritoPlanta = db.relationship("Favorites", backref="planets", cascade="all, delete-orphan", lazy=True)
 
     def __repr__(self):
         return '<Planets %r>' % self.id
@@ -58,9 +58,9 @@ class Planets(db.Model):
 
 class Favorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    id_user = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=True)
-    id_planets = db.Column(db.Integer, db.ForeignKey("planets.id"), nullable=False)
-    id_characters = db.Column(db.Integer, db.ForeignKey("characters.id"), nullable=False)
+    id_user = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    id_planets = db.Column(db.Integer, db.ForeignKey("planets.id"), nullable=True)
+    id_characters = db.Column(db.Integer, db.ForeignKey("characters.id"), nullable=True)
 
     def __repr__(self):
         return '<Favorites %r>' % self.id
